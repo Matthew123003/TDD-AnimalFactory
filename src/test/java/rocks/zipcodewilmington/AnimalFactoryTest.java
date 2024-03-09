549a88c2c2767ff60c66e1942ce4ad76f985fda3
Matt Kramer <matthew123003@gmail.com>
2024-03-09 11:48:26 -0500

    No idea

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,28 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Dog;
+
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void TestCreateDog(String name, Date birthDate) {
+        //Given
+        Date expectedDate = new Date(2024, Calendar.FEBRUARY, 1);
+        String expectedName = "Mittens";
+
+        //When
+        Animal dog = new Animal(expectedName, expectedDate)
+
+
+    }
 }
